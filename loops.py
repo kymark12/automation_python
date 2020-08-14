01ca42761e665cf8e70b8f322d151573f0a97085
ZenIvan <68838948+ZenIvan@users.noreply.github.com>
2020-08-15 04:32:12 +0800

    Added explanation on skipping index

--- loops.py
+++ loops.py
@@ -12,6 +12,8 @@ print(summation)
 
 print("*********************************")
 for k in range(1, 10, 5):
+    # third value is the number of skips example: 5 so 1 + 5 = 6 if you add 5 it will be over 10
+    # so it won't allow it to go over 10, so 6 is the last output (Check terminal after run)
     print(k)
     print("*****************SKIPPING FIRST INDEX**********************")
 for m in range(10):  # 0 - 9 output
