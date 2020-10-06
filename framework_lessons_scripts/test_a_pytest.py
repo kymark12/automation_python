8fbea6a908fea787c4fabfa5bf9ab804ad2fd595
ZenIvan <68838948+ZenIvan@users.noreply.github.com>
2020-10-07 00:59:57 +0800

    Updated pytest lesson file and .gitignore

--- framework_lessons_scripts/test_a_pytest.py
+++ framework_lessons_scripts/test_a_pytest.py
@@ -1,5 +1,16 @@
 # Any pytest file should start with test_, _test, _suite
-
+# Pytest method names should start with test_
+# Any code should be wrapped in method only
+# Method name should have sense
+# -k stands for method names execution, -s logs in output, -v stands for more info metadata
+# you can run specific file with pytest <file name>
 
 def test_first_program():
-    print("Hello")
+    msg = "Hello"
+    assert msg == "Hi", 'Test failed because strings do not match'
+
+
+def test_second_program():
+    a = 4
+    b = 6
+    assert a + 2 == b, "Addition does not match"
